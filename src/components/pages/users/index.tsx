f6500762a7cad8e5b2a02fbb55a3b2a71a4bb81d
Ravi Vemula <rvemula@greendotcorp.com>
2023-04-26 08:10:59 -0400

    Added rest endpoint integration to pull data

--- src/components/pages/users/index.tsx
+++ src/components/pages/users/index.tsx
@@ -1,12 +1,25 @@
 import React from 'react';
 import Form from "@rjsf/antd";
 import validator from '@rjsf/validator-ajv8';
+import  { useState, useEffect } from 'react';
 
 export const Users: React.FC = () => {
   const schema = require("./jsonFiles/schema.json")
-  const formData = require("./jsonFiles/formData.json")
   const uiSchema = require("./jsonFiles/uiSchema.json")
+  const [formData, setFormData] = useState({});
 
+  
+  useEffect(() => {
+    (async () => {
+      try {
+        const response = await fetch('https://api.fake-rest.refine.dev/users/1');
+        const responseBody = await response.json();
+        setFormData(responseBody);
+      } catch (err) {
+        console.error(err);
+      }
+    })();
+  }, []);
   return (
     <div className="editor-page">
       <div className="page container">
